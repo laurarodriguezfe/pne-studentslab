0b3bfddfbb50fda0cbe0b29da92aef6749eda11d
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-02-10 16:51:57 +0100

    dna-count exercises done!

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -23,11 +23,19 @@ print("A:", a, "\nC:", c, "\nT:", t, "\nG:", g)
 
 
 ############
-seq = input("Introduce the sequence: ").upper()
-print(f"Total length: {len(seq)}")
-bases = {"A": 0, "C": 0, "G": 0, "T": 0}
-for base in seq:
-    if base in bases:
-        bases[base] += 1
-for base, count in bases.items():
-    print(F"{base}: {count}")
+def count_bases(sequence):
+    bases = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for base in sequence:
+        if base in bases:
+            bases[base] += 1
+    return bases
+
+if __name__ == "__main__":
+
+    sequence = input("Introduce the sequence: ").upper()
+    print(f"Total length: {len(sequence)}")
+
+    result = count_bases(sequence)
+
+    for base, count in result.items():
+        print(f"{base}: {count}")
