ffb170e693b9b6d3e3ec5755fb5225cbd72aba3f
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-02-10 16:11:36 +0100

    dna-count exercise done!

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -19,3 +19,15 @@ def analyze(seq):
 dna = str(input("Enter a dna sequence:")).upper()
 lenght, a, c, t, g = analyze(dna)
 print("A:", a, "\nC:", c, "\nT:", t, "\nG:", g)
+
+
+
+############
+seq = input("Introduce the sequence: ").upper()
+print(f"Total length: {len(seq)}")
+bases = {"A": 0, "C": 0, "G": 0, "T": 0}
+for base in seq:
+    if base in bases:
+        bases[base] += 1
+for base, count in bases.items():
+    print(F"{base}: {count}")
