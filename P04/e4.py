662e2a80f680f524003458ba825f1019b679505e
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-03-18 18:46:40 +0100

    Practice 4 exercises done!

---
+++ P04/e4.py
@@ -0,0 +1,77 @@
+import socket
+import termcolor
+from pathlib import Path
+
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    lines = req.split('\n')
+
+    req_line = lines[0]
+    get_info = req_line.split(" ")
+    if len(get_info) > 1:
+        if get_info[1] == "/info/A":
+            body = Path("html/info/A.html").read_text()
+        elif get_info[1] == "/info/C":
+            body = Path("html/info/C.html").read_text()
+        elif get_info[1] == "/info/G":
+            body = Path("html/info/G.html").read_text()
+        elif get_info[1] == "/info/T":
+            body = Path("html/info/T.html").read_text()
+        else:
+            body = """
+            <!DOCTYPE html>
+            <html lang="en" dir="ltr">
+              <head>
+                <meta charset="utf-8">
+                <title>BLANK BODY</title>
+              </head>
+              <body style="background-color: white;">
+                <h1>NOT VALID</h1>
+              </body>
+            </html>
+            """
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    status_line = "HTTP/1.1 200 OK\n"
+
+    header = "Content-Type: text/html\n"
+
+    header += f"Content-Length: {len(body)}\n"
+
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+ls.bind((IP, PORT))
+
+ls.listen()
+
+print("Green server configured!")
+
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+        ls.close()
+        exit()
+    else:
+
+        process_client(cs)
+
+        cs.close()
