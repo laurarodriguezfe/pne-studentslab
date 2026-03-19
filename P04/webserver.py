58a92a0b504dae6f5c031ff9858d8892c03bf331
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-03-19 17:19:33 +0100

    Practice 4 exercises done!

---
+++ P04/webserver.py
@@ -0,0 +1,68 @@
+import socket
+import termcolor
+from pathlib import Path
+
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    lines = req.split('\n')
+
+    req_line = lines[0]
+    get_info = req_line.split(" ")
+    if len(get_info) > 1:
+        if get_info[1] == "/info/A":
+            body = Path("html/info/A.html").read_text()
+        elif get_info[1] == "/info/C":
+            body = Path("html/info/C.html").read_text()
+        elif get_info[1] == "/info/G":
+            body = Path("html/info/G.html").read_text()
+        elif get_info[1] == "/info/T":
+            body = Path("html/info/T.html").read_text()
+        elif get_info[1] == "/":
+            body = Path("html/index.html").read_text()
+        else:
+            body = Path("html/error.html").read_text()
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    status_line = "HTTP/1.1 200 OK\n"
+
+    header = "Content-Type: text/html\n"
+
+    header += f"Content-Length: {len(body)}\n"
+
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+ls.bind((IP, PORT))
+
+ls.listen()
+
+print("Green server configured!")
+
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+        ls.close()
+        exit()
+    else:
+
+        process_client(cs)
+
+        cs.close()
