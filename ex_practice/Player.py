1e5f34ef4708166a03a3a0b8f752d0a99e72ca7a
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-03-24 16:33:55 +0100

    Practice 4 exercises done!

--- ex_practice/Player.py
+++ ex_practice/Player.py
@@ -4,3 +4,10 @@ import termcolor
 IP = "127.0.0.1"
 PORT = 8080
 
+termcolor.cprint("--Welcome to the Number Guessing Game!--", "yellow")
+
+game_over = False
+
+while not game_over:
+    try:
+        user_input = input("\nEnter your guess(0-100):")
