a35e26eb41a50586a2208607bb4c134489b68766
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-04-07 16:21:01 +0200

    Practice 4 exercises done!

---
+++ S14/webserver-e3.py
@@ -0,0 +1,51 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+PORT = 8080
+
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        termcolor.cprint(self.requestline, 'green')
+
+        if self.path == "/" or self.path == "/index.html":
+            contents = Path("index.html").read_text()
+        elif self.path == "/green.html":
+            contents = Path("green.html").read_text()
+        elif self.path == "/blue.html":
+            contents = Path("blue.html").read_text()
+        elif self.path == "/pink.html":
+            contents = Path("pink.html").read_text()
+        else:
+            contents = Path("error.html").read_text()
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        self.end_headers()
+
+        self.wfile.write(contents.encode())
+
+        return
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
