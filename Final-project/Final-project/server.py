cc07fb0c38dc07bd24aa48669c5b406bd7a7e42a
laurarodriguezfe <l.rodriguezfe.2025@alumnos.urjc.es>
2026-05-05 16:48:01 +0200

    Final Project

--- Final-project/Final-project/server.py
+++ Final-project/Final-project/server.py
@@ -125,6 +125,145 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         contents = read_html_file("chromosome.html").render(info={"length": length})
 
                         self.send_response(200)
+            except Exception as e:
+                contents = f"<h1>Internal Error: {e}</h1>"
+                self.send_response(500)
+
+        elif path == "/geneLookup":
+            try:
+                params = parse_qs(url_path.query)
+                gene = params.get("gene", [""])[0]
+
+                if not gene:
+                    contents = Path("html/error.html").read_text()
+                    self.send_response(404)
+
+                else:
+                    data = get_data(f"/lookup/symbol/homo_sapiens/{gene}")
+                    gene_id = data.get("id", None)
+
+                    if gene_id is None:
+                        contents = Path("html/error.html").read_text()
+                        self.send_response(404)
+                    else:
+                        contents = read_html_file("geneLookup.html").render(info={"gene": gene, "id": gene_id})
+                        self.send_response(200)
+
+            except Exception as e:
+                contents = f"<h1>Internal Error: {e}</h1>"
+                self.send_response(500)
+
+        elif path == "/geneSeq":
+            try:
+                params = parse_qs(url_path.query)
+                gene = params.get("gene", [""])[0]
+
+                if not gene:
+                    contents = Path("html/error.html").read_text()
+                    self.send_response(404)
+
+                else:
+                    data = get_data(f"/lookup/symbol/homo_sapiens/{gene}")
+                    gene_id = data.get("id", None)
+
+                    if gene_id is None:
+                        contents = Path("html/error.html").read_text()
+                        self.send_response(404)
+                    else:
+                        data_seq = get_data(f"/sequence/id/{gene_id}")
+                        sequence = data_seq.get("seq", None)
+
+                        if sequence is None:
+                            contents = Path("html/error.html").read_text()
+                            self.send_response(404)
+                        else:
+                            contents = read_html_file("geneSeq.html").render(info={"gene": gene, "seq": sequence})
+                            self.send_response(200)
+
+            except Exception as e:
+                contents = f"<h1>Internal Error: {e}</h1>"
+                self.send_response(500)
+
+        elif path == "/geneInfo":
+            try:
+                params = parse_qs(url_path.query)
+                gene = params.get("gene", [""])[0]
+
+                if not gene:
+                    contents = Path("html/error.html").read_text()
+                    self.send_response(404)
+
+                else:
+                    data = get_data(f"/lookup/symbol/homo_sapiens/{gene}")
+                    gene_id = data.get("id", None)
+                    start = data.get("start", None)
+                    end = data.get("end", None)
+                    chromo = data.get("seq_region_name", None)
+
+                    if None in (gene_id, start, end, chromo):
+                        contents = Path("html/error.html").read_text()
+                        self.send_response(404)
+
+                    else:
+                        length = int(end) - int(start)
+                        contents = read_html_file("geneInfo.html").render(info={"gene": gene, "id": gene_id, "start": start, "end": end, "length": length, "chromo": chromo})
+
+                        self.send_response(200)
+
+            except Exception as e:
+                contents = f"<h1>Internal Error: {e}</h1>"
+                self.send_response(500)
+
+        elif path == "/geneCalc":
+            try:
+                params = parse_qs(url_path.query)
+                gene = params.get("gene", [""])[0]
+
+                if not gene:
+                    contents = Path("html/error.html").read_text()
+                    self.send_response(404)
+
+                else:
+                    data = get_data(f"/lookup/symbol/homo_sapiens/{gene}")
+                    gene_id = data.get("id", None)
+
+                    if gene_id is None:
+                        contents = Path("html/error.html").read_text()
+                        self.send_response(404)
+
+                    else:
+                        data_seq = get_data(f"/sequence/id/{gene_id}")
+                        sequence = data_seq.get("seq", None)
+
+                        if sequence is None:
+                            contents = Path("html/error.html").read_text()
+                            self.send_response(404)
+
+                        #################################################################################else:
+                            data_seq = get_data(f"/sequence/id/{gene_id}")
+                            sequence = data_seq.get("seq", None)
+                            if sequence is None:
+                                contents = Path("html/error.html").read_text()
+                                self.send_response(404)
+
+                            else:
+                                length = len(sequence)
+                                a = sequence.count("A")
+                                c = sequence.count("C")
+                                g = sequence.count("G")
+                                t = sequence.count("T")
+
+                                def percent(x):
+                                    return round((x / length) * 100, 2) if length > 0 else 0
+
+                                result = ""
+                                result += f"<li>A: {a} ({percent(a)}%)</li>"
+                                result += f"<li>C: {c} ({percent(c)}%)</li>"
+                                result += f"<li>G: {g} ({percent(g)}%)</li>"
+                                result += f"<li>T: {t} ({percent(t)}%)</li>"
+
+                                contents = read_html_file("geneCalc.html").render(info={"gene": gene, "id": gene_id, "length": length, "result": result})
+                                self.send_response(200)
 
             except Exception as e:
                 contents = f"<h1>Internal Error: {e}</h1>"
